c660c78ac2c2f4ac0ef16cb0ab2d63e442991951
GWeindel <gabriel.weindel@proton.me>
2025-03-16 18:28:49 +0100

    Improved testing with multilevel and starting point tests

--- tests/test_fixed.py
+++ tests/test_fixed.py
@@ -29,23 +29,21 @@ def init_data():
                    DATA_DIR_B / 'dataset_b_raw_raw_generating_events.npy']
     for file in event_files:
         events.append(np.load(file))
-
+    event_a = events[0]
     event_b = events[1]
     # Data reading
     epoch_data = hmp.utils.read_mne_data(raws, event_id=event_id, resp_id=resp_id, sfreq=sfreq,
-            events_provided=events, verbose=True, reference='average', high_pass=1, low_pass=45,
-                subj_idx=['a','b'], pick_channels='eeg', lower_limit_rt=0.01, upper_limit_rt=2)
+            events_provided=events, verbose=True, reference='average', subj_idx=['a','b'], tmin=-.01)
     epoch_data = epoch_data.assign_coords({'condition': ('participant', epoch_data.participant.data)})
     positions = simulations.simulation_positions()
     hmp_data = hmp.utils.transform_data(epoch_data, n_comp=2,)
-    return event_b, epoch_data, hmp_data, positions, sfreq, n_events
+    return event_b, event_a, epoch_data, hmp_data, positions, sfreq, n_events
 
 def test_fixed_simple():
     """ test a simple fit_transform on perfect data and compare to ground truth."""
-    event_b, epoch_data, hmp_data, positions, sfreq, n_events = init_data()
+    event_b, event_a, epoch_data, hmp_data, positions, sfreq, n_events = init_data()
     # Data b is without noise, recovery should be perfect
     data_b = hmp.utils.participant_selection(hmp_data, 'b')
-
     event_properties = EventProperties.create_expected(sfreq=data_b.sfreq)
     trial_data_b = TrialData.from_standard_data(data=data_b, template=event_properties.template)
     model = FixedEventModel(event_properties, n_events=n_events)
@@ -59,16 +57,16 @@ def test_fixed_simple():
     true_loglikelihood, true_estimates = model.transform(trial_data_b)
     true_topos = hmp.utils.event_topo(epoch_data, true_estimates.squeeze(), mean=True)
     #Estimate
-    lkh_b, estimates = model.fit_transform(trial_data_b, verbose=True)
-    test_topos = hmp.utils.event_topo(epoch_data, estimates.squeeze(), mean=True)
+    lkh_b, estimates_b = model.fit_transform(trial_data_b, verbose=True)
+    test_topos = hmp.utils.event_topo(epoch_data, estimates_b.squeeze(), mean=True)
     
     # Test if events found are classified as true
     assert (np.array(simulations.classification_true(true_topos,test_topos)) == np.array(([0,1,2],[0,1,2]))).all()
     # test the difference between electrode values at event times
     assert np.isclose(np.sum(np.abs(true_topos.data - test_topos.data)), 0, atol=1e-4, rtol=0)
     # Test whether likelihood is the expected one
-    assert np.isclose(lkh_b, np.array(7.45602626), atol=1e-4, rtol=0)
-
+    assert np.isclose(lkh_b, np.array(16.5514338), atol=1e-4, rtol=0)
+    
     # testing recovery of attributes
     model.xrlikelihoods
     model.xrmags
@@ -77,30 +75,49 @@ def test_fixed_simple():
     model.xrtraces
 
 def test_fixed_multilevel():
-    ...
-    # _, epoch_data, hmp_data, positions, sfreq, n_events = init_data()
-    # # Testing starting points
-    # model_sp = FixedEventModel(event_properties, n_events=n_events, starting_points=2, max_scale=21)
-    # model_sp.fit(trial_data_a, verbose=True)
-  
-    # # testing multilevel model
-    # mags_map = np.array([[0, 0, 0],
-    #                      [0, 0, 0]])
-    # pars_map = np.array([[0, 0, 0, 0],
-    #                      [0, 0, 1, 0],])
-    # level_dict = {'condition': ['a', 'b']}
-    # trial_data = TrialData.from_standard_data(data=hmp_data, template=event_properties.template)
-    # model.fit(trial_data, pars_map=pars_map, mags_map=mags_map, level_dict=level_dict)
+    _, _, epoch_data, hmp_data, positions, sfreq, n_events = init_data()
 
-    # # Check that levels actually work 
-    # lkh_a_level, estimates_a_level = model.transform(trial_data_a)
-    # trial_data_b = TrialData.from_standard_data(data=data_b, template=event_properties.template)
-    # lkh_b_level, estimates_b_level = model.transform(trial_data_a)
-    # print(lkh_a_level)
-    # print(lkh_a)
-    # assert lkh_a_level > lkh_a
+    # testing multilevel model
+    mags_map = np.array([[0, 0, 0],
+                         [0, 0, 0]])
+    pars_map = np.array([[0, 0, 0, 0],
+                         [0, 0, 1, 0],])
+    level_dict = {'condition': ['a', 'b']}
+    
+    hmp_data = hmp.utils.transform_data(epoch_data, n_comp=2,)
+    event_properties = EventProperties.create_expected(sfreq=hmp_data.sfreq)
+    hmp_data_a = hmp.utils.participant_selection(hmp_data, 'a')
+    hmp_data_b = hmp.utils.participant_selection(hmp_data, 'b')
+    trial_data = TrialData.from_standard_data(data=hmp_data, template=event_properties.template)
+    trial_data_a = TrialData.from_standard_data(data=hmp_data_a, template=event_properties.template)
+    trial_data_b = TrialData.from_standard_data(data=hmp_data_b, template=event_properties.template)
+
+    model = FixedEventModel(event_properties, n_events=n_events)
+    
+    # Perform a fit on a (should be too noisy)
+    lkh_a, estimates_a = model.fit_transform(trial_data_a)
+
+    # Fit model on both conditions (noiseless b should help estimate a)
+    trial_data = TrialData.from_standard_data(data=hmp_data, template=event_properties.template)
+    lkh_comb, estimates_comb = model.fit_transform(trial_data, pars_map=pars_map, mags_map=mags_map, level_dict=level_dict)
+    lkh_b_level, estimates_b_level = model.transform(trial_data_b)
+    lkh_a_level, estimates_a_level = model.transform(trial_data_a)
+
+    # Testing one event less in one condition
+    mags_map = np.array([[0, 0, 0],
+                         [0, 0, -1]])
+    pars_map = np.array([[0, 0, 0, 0],
+                         [0, 0, -1, 0],])
+    lkh_comb, estimates_comb = model.fit_transform(trial_data, pars_map=pars_map, mags_map=mags_map, level_dict=level_dict)
+    
+    assert lkh_a_level > lkh_a
     
 def test_starting_points():
-    ...
+    _, _, epoch_data, hmp_data, positions, sfreq, n_events = init_data()
+    event_properties = EventProperties.create_expected(sfreq=hmp_data.sfreq)
+    trial_data = TrialData.from_standard_data(data=hmp_data, template=event_properties.template)
+    # Testing starting points
+    model_sp = FixedEventModel(event_properties, n_events=n_events, starting_points=2, max_scale=21)
+    model_sp.fit(trial_data, verbose=True)
 
 

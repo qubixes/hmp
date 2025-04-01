6a065a6f4710b091103072811b80c1d8d40f0db5
Raoul Schram <r.d.schram@uu.nl>
2025-04-01 17:12:34 +0200

    Start work on backward estimation model

--- hmp/models/backward.py
+++ hmp/models/backward.py
@@ -24,8 +24,19 @@ from hmp.models.base import BaseModel
 from hmp.models.fixedn import FixedEventModel
 
 class BackwardEstimationModel(BaseModel):
+    def __init__(self, *args, max_events=None, min_events=0, max_starting_points=1,
+                 tolerance=1e-4, max_iteration=1e3, **kwargs):
+        self.max_events = max_events
+        self.min_events = min_events
+        self.max_starting_points = max_starting_points
+        self.tolerance = tolerance
+        self.max_iteration = max_iteration
+        self.submodels = {}
+        super().__init__(*args, **kwargs)
+
     def fit(
         self,
+        trial_data,
         max_events=None,
         min_events=0,
         base_fit=None,
@@ -61,24 +72,23 @@ class BackwardEstimationModel(BaseModel):
         max_iteration: int
             Maximum number of iteration for the expectation maximization in the EM() function
         """
-        fixed_n_model = FixedEventModel(self.trial_data, self.events, self.distribution)
-
         if max_events is None and base_fit is None:
-            max_events = self.compute_max_events()
+            max_events = self.compute_max_events(trial_data)
+        print("MAX_EVENTS", max_events)
         if not base_fit:
             if max_starting_points > 0:
                 print(
                     f"Estimating all solutions for maximal number of events ({max_events}) with 1 "
                     "pre-defined starting point and {max_starting_points - 1} starting points"
                 )
-            event_loo_results = [
-                fixed_n_model.fit(max_events, starting_points=max_starting_points, verbose=False)
-            ]
+            fixed_n_model = self.get_fixed_model(n_events=max_events)
+            event_loo_results = fixed_n_model.fit_transform(trial_data, verbose=False)
         else:
             event_loo_results = [base_fit]
         max_events = event_loo_results[0].event.max().values + 1
 
         for n_events in np.arange(max_events - 1, min_events, -1):
+            fixed_n_model = self.get_fixed_model(n_events)
             # only take previous model forward when it's actually fitting ok
             if event_loo_results[-1].loglikelihood.values != -np.inf:
                 print(f"Estimating all solutions for {n_events} events")
@@ -99,43 +109,33 @@ class BackwardEstimationModel(BaseModel):
                     pars_temp.append(temp_pars)
 
                 if cpus == 1:
-                    event_loo_likelihood_temp = []
                     for i in range(len(events_temp)):
-                        event_loo_likelihood_temp.append(
                             fixed_n_model.fit(
-                                n_events,
-                                events_temp[i],
-                                pars_temp[i],
+                                trial_data,
+                                magnitudes=events_temp[i],
+                                parameters=pars_temp[i],
                                 tolerance=tolerance,
                                 max_iteration=max_iteration,
-                                maximization=maximization,
                                 verbose=False,
                             )
-                        )
                 else:
                     inputs = zip(
-                        itertools.repeat(n_events),
-                        events_temp,
-                        pars_temp,
-                        itertools.repeat([]),
-                        itertools.repeat([]),
-                        itertools.repeat(tolerance),
-                        itertools.repeat(max_iteration),
-                        itertools.repeat(maximization),
-                        itertools.repeat(1),
-                        itertools.repeat(1),
-                        itertools.repeat(True),
-                        itertools.repeat(False),
-                        itertools.repeat(1),
+                        itertools.repeat(trial_data),
+                        events_temp,  # magnitudes
+                        pars_temp,  # parameters
+                        itertools.repeat(None),  # parameters_to_fix
+                        itertools.repeat(None),  # magnitudes_to_fix
+                        itertools.repeat(False),  # verbose
+                        itertools.repeat(1),  # cpus
                     )
                     with mp.Pool(processes=cpus) as pool:
-                        event_loo_likelihood_temp = pool.starmap(fixed_n_model.fit, inputs)
+                        pool.starmap(fixed_n_model.fit, inputs)
 
-                lkhs = [x.loglikelihood.values for x in event_loo_likelihood_temp]
-                event_loo_results.append(event_loo_likelihood_temp[np.nanargmax(lkhs)])
+                # lkhs = [x.loglikelihood.values for x in event_loo_likelihood_temp]
+                # event_loo_results.append(event_loo_likelihood_temp[np.nanargmax(lkhs)])
 
                 # remove event_loo_likelihood
-                del event_loo_likelihood_temp
+                # del event_loo_likelihood_temp
                 # Force garbage collection
                 gc.collect()
 
@@ -143,21 +143,31 @@ class BackwardEstimationModel(BaseModel):
                 print(
                     f"Previous model did not fit well. Estimating a neutral {n_events} event model."
                 )
-                event_loo_results.append(
-                    self.fixed_n_model(
-                        n_events,
-                        tolerance=tolerance,
-                        max_iteration=max_iteration,
-                        maximization=maximization,
-                    )
-                )
-        event_loo_results = xr.concat(event_loo_results, dim="n_events", fill_value=np.nan)
-        event_loo_results = event_loo_results.assign_coords(
-            {"n_events": np.arange(max_events, min_events, -1)}
-        )
-        event_loo_results = event_loo_results.assign_attrs(method="backward")
-        if "sp_parameters" in event_loo_results.attrs:
-            del event_loo_results.attrs["sp_parameters"]
-            del event_loo_results.attrs["sp_magnitudes"]
-            del event_loo_results.attrs["maximization"]
-        return event_loo_results
+            self.submodels[n_events] = fixed_n_model
+                # event_loo_results.append(
+                #     self.get_fixed_model(n_events)
+                # )
+    
+        # event_loo_results = xr.concat(event_loo_results, dim="n_events", fill_value=np.nan)
+        # event_loo_results = event_loo_results.assign_coords(
+        #     {"n_events": np.arange(max_events, min_events, -1)}
+        # )
+        # event_loo_results = event_loo_results.assign_attrs(method="backward")
+        # if "sp_parameters" in event_loo_results.attrs:
+        #     del event_loo_results.attrs["sp_parameters"]
+        #     del event_loo_results.attrs["sp_magnitudes"]
+        #     del event_loo_results.attrs["maximization"]
+        # return event_loo_results
+    def transform(self, trial_data):
+        if len(self.submodels) == 0:
+            raise ValueError("Model has not been (succesfully) fitted yet, no fixed models.")
+        return xr.concat([m.transform(trial_data) for m in self.submodels],
+                         dim="n_events", coords=list(self.submodels))
+
+
+    def get_fixed_model(self, n_events):
+        return FixedEventModel(
+            self.events, self.distribution, n_events=n_events,
+            starting_points=self.max_starting_points,
+            tolerance=self.tolerance,
+            max_iteration=self.max_iteration)
---
+++ tests/test_backward.py
@@ -0,0 +1,68 @@
+## Importing these packages is specific for this simulation case
+import os
+
+import numpy as np
+import pandas as pd
+from pathlib import Path
+import gc
+from pytest import mark
+
+import hmp
+from hmp import simulations
+from hmp.models import BackwardEstimationModel
+from hmp.models.base import EventProperties
+from hmp.trialdata import TrialData
+
+DATA_DIR = Path("tests", "gen_data")
+DATA_DIR_A = DATA_DIR / "dataset_a"
+DATA_DIR_B = DATA_DIR / "dataset_b"
+
+def init_data():
+    """ Initialize all data and model related info."""
+    sfreq = 100
+    n_events = 3
+    events = []
+    event_id = {'stimulus':1}#trigger 1 = stimulus
+    resp_id = {'response':5}
+    raws = [DATA_DIR_A / 'dataset_a_raw_raw.fif', DATA_DIR_B / 'dataset_b_raw_raw.fif']
+    event_files = [DATA_DIR_A / 'dataset_a_raw_raw_generating_events.npy',
+                   DATA_DIR_B / 'dataset_b_raw_raw_generating_events.npy']
+    for file in event_files:
+        events.append(np.load(file))
+    event_a = events[0]
+    event_b = events[1]
+    # Data reading
+    epoch_data = hmp.utils.read_mne_data(raws, event_id=event_id, resp_id=resp_id, sfreq=sfreq,
+            events_provided=events, verbose=True, reference='average', subj_idx=['a','b'], tmin=-.01)
+    epoch_data = epoch_data.assign_coords({'condition': ('participant', epoch_data.participant.data)})
+    positions = simulations.simulation_positions()
+    hmp_data = hmp.utils.transform_data(epoch_data, n_comp=2,)
+    return event_b, event_a, epoch_data, hmp_data, positions, sfreq, n_events
+
+def test_fixed_simple():
+    """ test a simple fit_transform on perfect data and compare to ground truth."""
+    event_b, event_a, epoch_data, hmp_data, positions, sfreq, n_events = init_data()
+    # Data b is without noise, recovery should be perfect
+    data_b = hmp.utils.participant_selection(hmp_data, 'b')
+    event_properties = EventProperties.create_expected(sfreq=data_b.sfreq)
+    trial_data_b = TrialData.from_standard_data(data=data_b, template=event_properties.template)
+    model = BackwardEstimationModel(event_properties)
+    # Recover generating parameters
+    sim_source_times, true_pars, true_magnitudes, _ = \
+        simulations.simulated_times_and_parameters(event_b, model, trial_data_b)
+    # Fixing true parameter in model
+    model.parameters = np.array([true_pars])
+    model.magnitudes = np.array([true_magnitudes])
+
+    # fit the model
+    model.fit(trial_data_b)
+
+    # Transform the data
+    estimates = model.transform(trial_data_b)
+
+    # testing recovery of attributes
+    model.xrlikelihoods
+    model.xrmags
+    model.xrparams
+    model.param_dev
+    model.xrtraces

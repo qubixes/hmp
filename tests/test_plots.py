7b3ab33a9a9f8e58e1bf74ba3e97962f05c4cf75
GWeindel <gabriel.weindel@proton.me>
2025-04-03 15:33:54 +0200

    Adapted visu to new fit/transform architecture
    Also added tests (and slight modification of fixedn test)

--- tests/test_plots.py
+++ tests/test_plots.py
@@ -26,28 +26,28 @@ def test_plot():
     _, _, epoch_data, hmp_data, positions, sfreq, n_events = init_data()
     
     # Testing one event less in one condition
-    mags_map = np.array([[0, 0, 0],
-                         [0, 0, -1]])
-    pars_map = np.array([[0, 0, 0, 0],
-                         [0, 0, -1, 0],])
+    mags_map = np.array([[0, 0, -1],
+                         [0, 0, 0]])
+    pars_map = np.array([[0, 0, -1, 0],
+                         [0, 0, 0, 0],])
     level_dict = {'condition': ['a', 'b']}
     
     event_properties = EventProperties.create_expected(sfreq=hmp_data.sfreq)
-    hmp_data_a = hmp.utils.participant_selection(hmp_data, 'a')
+    hmp_data_b = hmp.utils.participant_selection(hmp_data, 'a')
     trial_data = TrialData.from_standard_data(data=hmp_data, template=event_properties.template)
-    trial_data_a = TrialData.from_standard_data(data=hmp_data_a, template=event_properties.template)
+    trial_data_b = TrialData.from_standard_data(data=hmp_data_b, template=event_properties.template)
 
     model = FixedEventModel(event_properties, n_events=n_events)
     
     # Perform a fit on a (should be too noisy)
-    lkh_a, estimates_a = model.fit_transform(trial_data_a)
+    lkh_b, estimates_b = model.fit_transform(trial_data_b)
 
     # Fit model on both conditions (noiseless b should help estimate a)
     trial_data = TrialData.from_standard_data(data=hmp_data, template=event_properties.template)
     lkh_comb, estimates_comb = model.fit_transform(trial_data, pars_map=pars_map, mags_map=mags_map, level_dict=level_dict)
-    lkh_a_level, estimates_a_level = model.transform(trial_data_a)
+    lkh_b_level, estimates_b_level = model.transform(trial_data_b)
 
-    plot_topo_timecourse(epoch_data, estimates_comb, positions, as_time=True, 
+    plot_topo_timecourse(epoch_data, estimates_comb, positions, as_time=True, colorbar=False, )
+    plot_topo_timecourse(epoch_data, estimates_b, positions, as_time=True, 
                        max_time=500, colorbar=False, )
-    plot_topo_timecourse(epoch_data, estimates_a, positions, as_time=True, 
-                       max_time=500, colorbar=False, )
+    plt.show()

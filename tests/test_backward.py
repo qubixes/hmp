6b4062e80ae3cf7b2df86afc19b05fc89aeb5ab4
GWeindel <gabriel.weindel@proton.me>
2025-04-04 13:23:11 +0200

    Added assertion to test for correct recovery
    (Failing for now)

--- tests/test_backward.py
+++ tests/test_backward.py
@@ -11,6 +11,7 @@ import xarray as xr
 import hmp
 from hmp import simulations
 from hmp.models import BackwardEstimationModel
+from hmp.models import FixedEventModel
 from hmp.models.base import EventProperties
 from hmp.trialdata import TrialData
 
@@ -47,20 +48,27 @@ def test_fixed_simple():
     data_b = hmp.utils.participant_selection(hmp_data, 'b')
     event_properties = EventProperties.create_expected(sfreq=data_b.sfreq)
     trial_data_b = TrialData.from_standard_data(data=data_b, template=event_properties.template)
-    model = BackwardEstimationModel(event_properties)
+    
+    true_model = FixedEventModel(event_properties, n_events=n_events)
     # Recover generating parameters
     sim_source_times, true_pars, true_magnitudes, _ = \
-        simulations.simulated_times_and_parameters(event_b, model, trial_data_b)
+        simulations.simulated_times_and_parameters(event_b, true_model, trial_data_b)
     # Fixing true parameter in model
-    model.parameters = np.array([true_pars])
-    model.magnitudes = np.array([true_magnitudes])
+    true_model.parameters = np.array([true_pars])
+    true_model.magnitudes = np.array([true_magnitudes])
+    # Ground truth
+    true_loglikelihood, true_estimates = true_model.transform(trial_data_b)
 
+    # Backward estimation
+    model = BackwardEstimationModel(event_properties)
     # fit the model
     model.fit(trial_data_b)
-
     # Transform the data
     estimates = model.transform(trial_data_b)
 
+    # testing if bacward identifies the 3 real events
+    assert np.isclose(model.submodels[3].magnitudes, true_estimates.magnitudes)
+    
     # testing recovery of attributes
     assert isinstance(model.xrlikelihoods, xr.DataArray)
     assert isinstance(model.xrmags, xr.DataArray)
